9c5b966c1960ace4d56e36a85c79a3270983737b
BeardBear <138500494+BeardBear33@users.noreply.github.com>
2025-11-29 15:52:32 +0100

    Position fix for log_info text

--- src/autoupdate.cpp
+++ src/autoupdate.cpp
@@ -275,9 +275,9 @@ public:
         fs::path dirInc  = sqlRoot / "updates_include";
         fs::path dirUpd  = sqlRoot / "updates";
 
-        LOG_INFO("gv.customs", "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
-        LOG_INFO("gv.customs", "┃ Instance Tools – Customs SQL Updater (auto)         ┃");
-        LOG_INFO("gv.customs", "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+        LOG_INFO("gv.customs", "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        LOG_INFO("gv.customs", "┃ Instance Tools – Customs SQL Updater ┃");
+        LOG_INFO("gv.customs", "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
         LOG_INFO("gv.customs", "[customs] Module: {}", moduleName);
         LOG_INFO("gv.customs", "[customs] SQL root: {}", sqlRoot.string());
 
